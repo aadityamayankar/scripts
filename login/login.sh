740d1cae334b06b0b99e0f48f72b46b657f00385
Mayankar <53344662+aadityamayankar@users.noreply.github.com>
2023-03-08 20:07:36 +0530

    Update login.sh

--- login/login.sh
+++ login/login.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+# Login credentials
 username="<USERNAME>"
 password="<PASSWORD>"
-dst="http%3A%2F%[2Fdetectportal.firefox.com](http://2fdetectportal.firefox.com/)%2Fcanonical.html"
-popup="true"
 
 # URL to send the POST request to
-
 url="<URL>"
+
+dst="http%3A%2F%[2Fdetectportal.firefox.com](http://2fdetectportal.firefox.com/)%2Fcanonical.html"
+popup="true"
 
 body="username=$username&password=$password&dst=$dst&popup=$popup"
 
